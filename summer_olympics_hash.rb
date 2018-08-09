7ae2457c818bf7e1ba67b8b53f838d271c6e682a
seamus-quinn <sumaesquinn@gmail.com>
2018-08-09 11:59:00 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,7 +13,7 @@ end
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
     updated_hash = create_olympics_hash
-    updated_hash[:Atlanta] = "1994"
+    updated_hash[:Atlanta] = "1996"
     return updated_hash
 end
 
